ed44940a92037c6230530ac7097b82ac00edbf8f
AlexIrungu <aleaxmuiruri@gmail.com>
2023-01-27 10:03:11 +0300

    Completed components and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import MarsWeather from './Components/MarsWeather';
+import NasaPicture from './Components/NasaPIcture';
 import NearEarthObject from './Components/NearEarthObject';
+import './Components/MarsWeather.css'
+import './Components/NasaPIcture.css'
 
 function App (){
 
 return (
   <div>
     <NearEarthObject />
+    <NasaPicture />
+    <MarsWeather />
   </div>
 )
 }
