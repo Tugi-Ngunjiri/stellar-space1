3cd035b04a6d72450260ea39e1247169f32d8429
Student <venus.kabera@student.moringaschool.com>
2023-01-27 09:49:08 +0300

    merged the repos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import NearEarthObject from './Components/NearEarthObject';
+import Images from ".//Components/Images"
 
 function App (){
 
 return (
   <div>
     <NearEarthObject />
+    <Images/>
   </div>
 )
 }
