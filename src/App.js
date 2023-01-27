9fbfe0510322d3a82b85c40db6319d526dfbc632
tugi Ngunjiri <102291617+Tugi-Ngunjiri@users.noreply.github.com>
2023-01-27 09:31:58 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import NearEarthObject from './Components/NearEarthObject';
+
+function App (){
+
+return (
+  <div>
+    <NearEarthObject />
+  </div>
+)
+}
+
+
+
+export default App;
