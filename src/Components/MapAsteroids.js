6c407cc4bff94c42644082bae3a0acc8bc360366
AlexIrungu <aleaxmuiruri@gmail.com>
2023-01-28 12:46:57 +0300

    routing components completed

--- src/Components/MapAsteroids.js
+++ src/Components/MapAsteroids.js
@@ -4,7 +4,7 @@ import React from "react";
 function MapAsteroids(){
     return(
         <div className="space">
-            <iframe className="map" width="100%" height="100vh" seamless="seamless"  src="https://eyes.nasa.gov/apps/asteroids/" frameBorder="0"></iframe>
+            <iframe className="map" width="100%" height="100vh" seamless="seamless"  src="https://eyes.nasa.gov/apps/asteroids/" ></iframe>
 
         </div>
     )
